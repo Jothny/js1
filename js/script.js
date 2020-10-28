c4c07f7a689001716bd1ead1e0eaa1d3a4e982e3
Jothny <kozin.dimka.gl@gmail.com>
2020-10-28 21:41:40 +0200

    finish movie app

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 "use strict";
 
-const numberOfFilms = prompt('Сколько фильмов ты уже посмотрел?', ''),
+const numberOfFilms = +prompt('Сколько фильмов ты уже посмотрел?', ''),
     personalMovieDB = {
         count: numberOfFilms,
         movies: {},
         actors: {},
         genres: [],
         privat: false
-    }
+    };
+
+let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оценишь его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оценишь его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
 
-console.log(numberOfFilms);
+console.log(personalMovieDB);
